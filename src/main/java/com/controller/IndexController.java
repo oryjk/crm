1c9fa8b81bc533be06b464ca7db614c43497ef28
Carl Wang <oryjk@qq.com>
2016-05-10 22:19:10 +0800

    add

---
+++ src/main/java/com/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by carlwang on 5/10/16.
+ */
+@RequestMapping("/")
+@RestController
+public class IndexController {
+
+    @RequestMapping
+    public ModelAndView index(ModelAndView modelAndView) {
+        modelAndView.setViewName("/index");
+        return modelAndView;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/jsp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: carlwang
+  Date: 5/10/16
+  Time: 10:15 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+Hello World!!!
+</body>
+</html>
