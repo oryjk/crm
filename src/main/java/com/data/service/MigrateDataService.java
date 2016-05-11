c2cb921a35aa05cf578441a7698a51f7b0133bd4
Carl Wang <oryjk@qq.com>
2016-05-11 20:49:02 +0800

    add

--- src/main/java/com/data/service/MigrateDataService.java
+++ src/main/java/com/data/service/MigrateDataService.java
@@ -26,7 +26,7 @@ public class MigrateDataService {
             Flyway flyway = new Flyway();
             flyway.setDataSource(dataSource);
 
-            flyway.setSchemas("crm"); // 设置接受flyway进行版本管理的多个数据库
+            flyway.setSchemas("jxc"); // 设置接受flyway进行版本管理的多个数据库
             flyway.setTable("schema_version"); // 设置存放flyway metadata数据的表名
             flyway.setLocations("/db/migration"); // 设置flyway扫描sql升级脚本、java升级脚本的目录路径或包路径
             flyway.setEncoding("UTF-8"); // 设置sql脚本文件的编码
