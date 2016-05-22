f8dd1e27ad01ed0cfe4003f8e4568b65547a07db
Carl Wang <oryjk@qq.com>
2016-05-22 14:18:16 +0800

    add

--- jxc/system/database/drivers/mysql/mysql_driver.php
+++ jxc/system/database/drivers/mysql/mysql_driver.php
@@ -83,6 +83,7 @@ class CI_DB_mysql_driver extends CI_DB
      */
     function db_pconnect()
     {
+        $this->port='3306';
         if ($this->port != '') {
             $this->hostname .= ':' . $this->port;
         }
